d677cc269d1f8a874b54657f5ce037fc0ce25ca8
Jaya DeHart <dehartj97@gmail.com>
2021-09-13 10:09:42 -0700

    init

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const dynamoose = require('dynamoose');
+
+const personSchema = new dynamoose.Schema({
+  primarykey: String,
+  name: String,
+  age: Number,
+  nickName: String,
+});
+
+const Person = dynamoose.model('lab-18-people', personSchema);
+
 exports.handler = async (event) => {
-  console.log(event);
-  return { message: 'test message' };
+  let updateObject = event.body;
+  let { id } = event.pathParameters;
+  const response = await Person.update({ primarykey: id }, { ...updateObject });
+  return {
+    message: 'updated-user',
+    user: response,
+  };
 };
